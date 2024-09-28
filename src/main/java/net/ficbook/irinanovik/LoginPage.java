7ac4e6960bf103ff3432c7406691a07a860f7098
Irina Novik <novikirishka7@gmail.com>
2024-09-28 20:32:11 +0300

    Add LoginPage

---
+++ src/main/java/net/ficbook/irinanovik/LoginPage.java
@@ -0,0 +1,11 @@
+package net.ficbook.irinanovik;
+
+public class LoginPage {
+
+    public static final String INPUT_USERNAME_XPATH = "//*[@id="username"]";
+    public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"password\"]";
+    public static final String BUTTON_ENTER_XPATH = "/html/body/div[1]/div/div[2]/div/div[1]/div[2]/form/button";
+    public static final String ERROR_USERNAME_XPATH = "//*[@id=\"username\"],'Логин не существует')]";
+    public static final String ERROR_PASSWORD_XPATH = "//*[@id=\"password\"],'Логин не существует')]";
+
+}
